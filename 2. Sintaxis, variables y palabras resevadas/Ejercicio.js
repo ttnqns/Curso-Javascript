cdb12b31da9298ce0839e0f839e8e6413bdc4dac
Tatiana Quiñones <admin.eva1@plataformaeva.cl>
2022-12-23 10:31:59 -0300

    create exercise

---
+++ 2. Sintaxis, variables y palabras resevadas/Ejercicio.js
@@ -0,0 +1,18 @@
+const Libro = {
+    titulo: "Coraline",
+    autor: "Neil Gaiman",
+    fecha: "2 de julio de 2002",
+    url: "https://www.amazon.com/-/es/Neil-Gaiman/dp/841863703X/ref=sr_1_1?keywords=coraline+espa%C3%B1ol&qid=1671802158&s=books&sprefix=coraline+es%2Cstripbooks-intl-ship%2C192&sr=1-1"
+}
+
+const lista = [
+    "Tatiana quiñones",
+    23,
+    true,
+    new Date("october 20 1999"),
+    Libro
+]
+
+
+
+console.log(lista);
